d11ba3690968ced9335f487dbe9a1815b15a0a15
覃湘 <qinx@smartsalary.cn>
2020-03-29 20:24:00 +0800

    hack 正则验证

---
+++ extensions/content-type-builder/admin/src/containers/FormModal/utils/attributesRegexes.js
@@ -0,0 +1,5 @@
+const CATEGORY_NAME_REGEX = new RegExp('^[\u4e00-\u9fa5_0-9A-Za-z]*$');
+const ENUM_REGEX = new RegExp('^[\u4e00-\u9fa5_0-9A-Za-z]*$');
+const NAME_REGEX = new RegExp('^[\u4e00-\u9fa5_0-9A-Za-z]*$');
+
+export { CATEGORY_NAME_REGEX, ENUM_REGEX, NAME_REGEX };
