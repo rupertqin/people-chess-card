90e3e13a916584acecfc24eef5acfbefa71d95eb
覃湘 <qinx@smartsalary.cn>
2020-03-29 20:44:44 +0800

    自定义 find

--- api/information/controllers/information.js
+++ api/information/controllers/information.js
@@ -1,8 +1,24 @@
 'use strict';
+const { sanitizeEntity } = require('strapi-utils');
+const omit = require('lodash/omit');
+
 
 /**
  * Read the documentation (https://strapi.io/documentation/3.0.0-beta.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async find(ctx) {
+    let entities;
+    if (ctx.query._q) {
+      entities = await strapi.services.information.search(ctx.query);
+    } else {
+      entities = await strapi.services.information.find(ctx.query);
+    }
+
+    return entities.map(entity =>
+      omit(sanitizeEntity(entity, { model: strapi.models.information }), ['content'])
+    );
+  },
+};
