40decb99f935df1162b3f457e26907c9761313e3
覃湘 <qinx@smartsalary.cn>
2020-04-25 16:15:32 +0800

    ck editor with upload

--- extensions/content-manager/admin/src/components/CKEditor/index.js
+++ extensions/content-manager/admin/src/components/CKEditor/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import CKEditor from '@ckeditor/ckeditor5-react';
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import ClassicEditor from 'ckeditor-build-with-simple-upload-provider-strapi';
 import styled from 'styled-components';
+import { auth } from 'strapi-helper-plugin';
 
 const Wrapper = styled.div`
   .ck-editor__main {
@@ -14,18 +15,29 @@ const Wrapper = styled.div`
 `;
 
 const Editor = ({ onChange, name, value }) => {
+
+  const jwtToken = auth.getToken();
+
   return (
     <Wrapper>
-      <CKEditor
-        editor={ClassicEditor}
-        data={value}
-        onChange={(event, editor) => {
-          const data = editor.getData();
-          onChange({ target: { name, value: data } });
-        }}
-      />
-    </Wrapper>
-  );
+    <CKEditor
+  editor={ClassicEditor}
+  data={value}
+  onChange={(event, editor) => {
+    const data = editor.getData();
+    onChange({ target: { name, value: data } });
+  }}
+  config={{
+    simpleUpload: {
+      uploadUrl: `${strapi.backendURL}/upload`,
+        headers: {
+          Authorization: "Bearer " + jwtToken
+      }
+    }
+  }}
+  />
+  </Wrapper>
+);
 };
 
 Editor.propTypes = {
